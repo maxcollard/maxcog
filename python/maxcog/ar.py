d4b388ffbc84ae81ae927c29f87a8d809b04a698
Max Collard <collard@jhu.edu>
2017-04-10 14:48:01 -0400

    Added old-skool tvdbn

--- python/maxcog/ar.py
+++ python/maxcog/ar.py
@@ -25,6 +25,58 @@ def _tvdbn_kernel ( ts, t0, h ):
 def tvdbn_labeled( X, **kwargs ):
     pass
 
+def tvdbn2 ( X,
+             step = 1,
+             predict_lag = 1,
+             reg_weight = 1.0,
+             kernel_width = 4.0,
+             kernel_thresh = 0.001,
+             show_pbar = False):
+    ''' ... '''
+
+    # Shortcuts
+    T = X.shape[0]
+    N = X.shape[1]
+    ts = np.arange( T )
+    
+    # Pre-allocate output
+    t_out = np.arange( predict_lag, T, step )
+    ret = np.zeros( (N, N, t_out.shape[0]) )
+    
+    # TODO Correct mapping between lambda and alpha??
+    model = lm.Ridge( alpha = reg_weight )
+    
+    t_iterator = pbar( range( t_out.shape[0] ) ) if show_pbar else range( t_out.shape[0] )
+    for i_t in t_iterator:
+        t = t_out[i_t]
+
+        # Compute weighting kernel
+        cur_kernel = _tvdbn_kernel( ts, t, kernel_width )
+        # Create slicing vectors for present and past, using threshold to cut fat
+        kernel_slice = np.nonzero( cur_kernel[predict_lag:] > kernel_thresh )[0]
+        kernel_slice_prev = kernel_slice - predict_lag
+        
+        for ch in range( N ):
+            # "All other channels"
+            ch_slice = np.arange( N ) != ch
+            
+            # Perform regression
+            Xt = X[kernel_slice_prev,:][:,ch_slice]
+            for ch_x in range( N - 1 ):
+                Xt[:, ch_x] = np.multiply( cur_kernel[kernel_slice], Xt[:, ch_x] )
+
+            Yt = X[kernel_slice,:][:,ch]
+            Yt = np.multiply( cur_kernel[kernel_slice], Yt )
+
+            result = np.dot( np.dot( np.linalg.pinv( ( np.dot( Xt.T, Xt ) ) + reg_weight * np.eye( N - 1 ) ),
+                                     Xt.T ),
+                             Yt.T )
+            
+            # Add regression output to return value
+            ret[ch, ch_slice, i_t] = result
+    
+    return ret
+
 def tvdbn ( X,
             step = 1,
             predict_lag = 1,
